4764e40664ee4c62b95b4030c806311c52adfb97
kanyunj <62997525+kanyunj@users.noreply.github.com>
2022-09-30 14:56:40 +0300

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -4,7 +4,7 @@
 
  export async function onRequestPost(context) {
     try {
-      let input = await context.request;
+      let input = await context.request.formData();
       let pretty = JSON.stringify([...input], null, 2);
       return new Response(pretty, {
         headers: {
